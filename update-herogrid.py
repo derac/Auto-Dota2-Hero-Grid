6c1579be826a9e9bde4a59ebb22a4e028ba22976
derac <dwolson123@gmail.com>
2020-09-24 08:49:45 -0500

    minor refactoring

--- update-herogrid.py
+++ update-herogrid.py
@@ -1,15 +1,13 @@
-import argparse, requests, json, os
-from datetime import date
+import argparse, requests, json, os, datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-path','-p',type=str,required=True,help="Required. STEAM_INSTALL/userdata/USER_ID/570/remote/cfg/hero_grid_config.json")
 parser.print_help(); print(); args = parser.parse_args()
 
-date_str = date.today().strftime(" %d-%m-%Y")
+date_str = datetime.date.today().strftime(" %d-%m-%Y")
 spec_url = 'https://stats.spectral.gg/lrg2/api/?pretty&league=imm_ranked_meta_last_7&mod=heroes-positions-position_'
 spec_positions = {"Core Safelane":"1.1","Core Midlane":"1.2","Core Offlane":"1.3","Support":"0.0"}
-rank_cutoffs = [100,97,93,90,85,80]
-tiers = ["S","A","B","C","D"]
+rank_cutoffs = [100,97,93,90,85,80]; tiers = ["S","A","B","C","D"]
 
 # open grid config and delete existing if desired
 if os.path.isfile(args.path):
@@ -19,8 +17,8 @@ if os.path.isfile(args.path):
         print("Grid config loaded.")
     except: print("Couldn't load the grid config."); quit()
 else:
-    print("Creating new Grid Config file.")
     grid_conf = {"version":3,"configs":[]}
+    print("Creating new Grid Config file.")
 
 # update the grid config with each position's data from spectral.gg
 for pos_name,pos_endpoint in spec_positions.items():
@@ -34,9 +32,10 @@ for pos_name,pos_endpoint in spec_positions.items():
                                 "hero_ids":[id for rank,id in hero_ranks if rank_cutoffs[i] >= rank > rank_cutoffs[i+1]]}
                                for i in range(5)]}
     grid_conf["configs"].append(pos_conf)
+    print("Processed",pos_name+'.')
 
 # write grid conf
 try:
     with open(args.path, "w") as f: print(json.dumps(grid_conf, indent=4), file=f)
-    print("\nGrid Config has been written.")
-except: print("\nCouldn't write the grid config.")
+    print("Grid Config has been written.")
+except: print("Couldn't write the grid config.")
