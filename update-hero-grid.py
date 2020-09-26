568cf1ef5a24f73080ab735ff9c37b156070cd5a
derac <dwolson123@gmail.com>
2020-09-26 01:46:53 -0500

    change layout

--- update-hero-grid.py
+++ update-hero-grid.py
@@ -2,12 +2,12 @@ import argparse, requests, json, os, datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-path','-p',type=str,required=True,help="Required. STEAM_INSTALL/userdata/USER_ID/570/remote/cfg/hero_grid_config.json")
+parser.add_argument('-verbose','-v',action="store_true",help="Show debug info.")
 parser.print_help(); print(); args = parser.parse_args()
 
 date_str = datetime.date.today().strftime(" %d-%m-%Y")
 spec_url = 'https://stats.spectral.gg/lrg2/api/?pretty&league=imm_ranked_meta_last_7&mod=heroes-positions-position_'
 spec_positions = {"Core Safelane":"1.1","Core Midlane":"1.2","Core Offlane":"1.3","Support":"0.0"}
-rank_cutoffs = [100,95,90,85,80,75]; tiers = ["S","A","B","C","D"]
 
 # open grid config and delete existing if desired
 if os.path.isfile(args.path):
@@ -24,18 +24,17 @@ else:
 for pos_name,pos_endpoint in spec_positions.items():
     try: hero_data = json.loads(requests.get(spec_url+pos_endpoint).content)["result"][pos_endpoint]
     except: print("\nFailed to load data from spectral.gg"); quit()
+    
     hero_ranks = [(data["rank"],hero_id) for hero_id,data in hero_data.items()]
     hero_ranks.sort(key=lambda x:-x[0])
     pos_conf = {"config_name": 'S! ' + pos_name + date_str,
-                "categories": [{"category_name":tiers[i]+" tier - rank %s+"%rank_cutoffs[i+1],
-                                "x_position":0, "y_position":i*120,"width":600,"height":100,
-                                "hero_ids":[id for rank,id in hero_ranks if rank_cutoffs[i] >= rank > rank_cutoffs[i+1]]}
-                               for i in range(5)]}
-    pos_conf["categories"].append({"category_name":"E tier - rank 50+",
-                                   "x_position":600, "y_position":0,"width":600,"height":300,
-                                   "hero_ids":[id for rank,id in hero_ranks if rank_cutoffs[-1] >= rank > 50]})
+                "categories": [{"category_name":chr(65+i)+" tier - rank %s+"%(100-5*i-5),
+                                "x_position":i//5*400, "y_position":(i%5)*120,"width":400,"height":100,
+                                "hero_ids":[id for rank,id in hero_ranks if (100-5*i) >= rank > (100-5*i-5)]}
+                               for i in range(15)]}
     grid_conf["configs"].append(pos_conf)
     print("Processed",pos_name+'.')
+    if args.verbose: print(pos_conf)
 
 # write grid conf
 try:
