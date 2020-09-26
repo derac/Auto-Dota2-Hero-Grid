dd7b18eedfed21c79b1fb860921c2b9a9112d111
derac <dwolson123@gmail.com>
2020-09-26 02:08:40 -0500

    refactor

--- update-hero-grid.py
+++ update-hero-grid.py
@@ -4,20 +4,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-path','-p',type=str,required=True,
                     help="located at c:\\{STEAM_INSTALL_LOCATION}\\userdata\\{STEAM_USER_ID}\\570\\remote\\cfg\\hero_grid_config.json")
 parser.add_argument('-verbose','-v',action="store_true",help="show debug info")
-args = parser.parse_args(); parser.print_help(); print(); 
-
+args = parser.parse_args(); parser.print_help(); print();
 date_str = datetime.date.today().strftime(" %d-%m-%Y")
 spec_url = 'https://stats.spectral.gg/lrg2/api/?league=imm_ranked_meta_last_7&mod=heroes-positions-position_'
-spec_positions = {"Core Safelane":"1.1","Core Midlane":"1.2","Core Offlane":"1.3","Support":"0.0"}
+spec_pos = {"Core Safelane":"1.1","Core Midlane":"1.2","Core Offlane":"1.3","Support":"0.0"}
 
 if os.path.isfile(args.path): # open grid config and delete existing if desired
     try:
         with open(args.path) as f: grid_conf = json.load(f); print("Grid config loaded.")
         grid_conf["configs"] = [c for c in grid_conf["configs"] if "S!" != c["config_name"][:2]]
-    except: print("Couldn't load the grid config."); quit()
+    except: print("Couldn't load the grid config."); quit();
 else: grid_conf = {"version":3,"configs":[]}; print("Creating new Grid Config file.")
 
-for pos_name,pos_endpoint in spec_positions.items(): # update the grid config
+for pos_name,pos_endpoint in spec_pos.items(): # update the grid config
     try: hero_data = json.loads(requests.get(spec_url+pos_endpoint).content)["result"][pos_endpoint]
     except: print("\nFailed to load data from spectral.gg"); quit()
     hero_ranks = sorted([(data["rank"],hero_id) for hero_id,data in hero_data.items()], key=lambda x:-x[0])
@@ -26,7 +25,7 @@ for pos_name,pos_endpoint in spec_positions.items(): # update the grid config
                                 "x_position":i//5*400, "y_position":(i%5)*120,"width":400,"height":100,
                                 "hero_ids":[id for rank,id in hero_ranks if (100-5*i) >= rank > (100-5*i-5)]}
                                for i in range(15)]}
-    grid_conf["configs"].append(pos_conf); print("Processed",pos_name+'.')
+    grid_conf["configs"].append(pos_conf); print("Processed",pos_name+'.');
     if args.verbose: print(pos_conf)
 
 try: # write grid conf
