df867610a1bb3f85689f6fb8d5b448006af7a5ea
derac <dwolson123@gmail.com>
2020-09-26 18:46:04 -0500

    change command structure to make it easier for windows users

--- update-hero-grid.py
+++ update-hero-grid.py
@@ -1,20 +1,24 @@
 import argparse, requests, json, os, datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-path','-p',type=str,required=True,
-                    help="located at c:\\{STEAM_INSTALL_LOCATION}\\userdata\\{STEAM_USER_ID}\\570\\remote\\cfg\\hero_grid_config.json")
+parser.add_argument('-steam_install_path','-p',type=str,
+                    help="If you don't enter the Steam install path, the script will try to get it from the Windows registry.")
+parser.add_argument('-user_id','-u',type=str,
+                    help="If you don't enter the user_id, the script will try to write to every user on the system.")
 parser.add_argument('-verbose','-v',action="store_true",help="show debug info")
 args = parser.parse_args(); parser.print_help(); print();
+
+if not args.steam_install_path:
+    print("Attempting to get Steam install path from registry."); import winreg
+    steam_path = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\WOW6432Node\Valve\Steam"), "InstallPath")[0]
+else: steam_path = args.steam_install_path
+
 date_str = datetime.date.today().strftime(" %d-%m-%Y")
 spec_url = 'https://stats.spectral.gg/lrg2/api/?league=imm_ranked_meta_last_7&mod=heroes-positions-position_'
 spec_pos = {"Core Safelane":"1.1","Core Midlane":"1.2","Core Offlane":"1.3","Support":"0.0"}
 
-if os.path.isfile(args.path): # open grid config and delete existing if desired
-    with open(args.path) as f: grid_conf = json.load(f); print("Grid config loaded.")
-    grid_conf["configs"] = [c for c in grid_conf["configs"] if "S!" != c["config_name"][:2]]
-else: grid_conf = {"version":3,"configs":[]}; print("Creating new Grid Config file.");
-
-for pos_name,pos_endpoint in spec_pos.items(): # update the grid config
+pos_confs = [] # Create tier configs from spectral.gg
+for pos_name,pos_endpoint in spec_pos.items(): 
     hero_data = json.loads(requests.get(spec_url+pos_endpoint).content)["result"][pos_endpoint]
     hero_ranks = sorted([(data["rank"],hero_id) for hero_id,data in hero_data.items()], key=lambda x:-x[0])
     pos_conf = {"config_name": 'S! ' + pos_name + date_str,
@@ -22,7 +26,16 @@ for pos_name,pos_endpoint in spec_pos.items(): # update the grid config
                                 "x_position":i//5*400, "y_position":(i%5)*120,"width":400,"height":100,
                                 "hero_ids":[id for rank,id in hero_ranks if (100-5*i) >= rank > (100-5*i-5)]}
                                for i in range(15)]}
-    grid_conf["configs"].append(pos_conf); print("Processed",pos_name+'.');
+    pos_confs.append(pos_conf); print("Processed",pos_name+'.');
     if args.verbose: print(pos_conf)
 
-with open(args.path, "w") as f: print(json.dumps(grid_conf, indent=4), file=f); print("Grid Config has been written.");
+for user_id in (os.listdir(os.path.join(steam_path,"userdata")) if not args.user_id else [args.user_id]):
+    path = os.path.join(steam_path,"userdata",user_id,"570","remote","cfg")
+    if not os.path.isdir(path): continue
+    conf_file = os.path.join(path,"hero_grid_config.json")
+    if os.path.isfile(conf_file): # open grid config and delete existing if desired
+        with open(conf_file) as f: grid_conf = json.load(f); print("Grid config loaded.")
+        grid_conf["configs"] = [c for c in grid_conf["configs"] if "S!" != c["config_name"][:2]] + pos_confs
+    else: grid_conf = {"version":3,"configs":[]}; print("Creating new Grid Config file.");
+
+    with open(conf_file, "w") as f: print(json.dumps(grid_conf, indent=4), file=f); print("Grid Config written.",conf_file);
